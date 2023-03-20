3d415a43b10f6aeeea44dc76a4e4f9798685b306
chelo7717 <chelo336@gmail.com>
2023-03-20 10:30:03 -0400

    mostrar Buzz cuando son multiplo de 5

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,8 +1,8 @@
 function fizzbuzz(num) {
-  if(num == 3){
+  if(num % 3 == 0){
     return "Fizz";
   }
-  if(num == 5){
+  if(num % 5 == 0){
     return "Buzz";
   }
   return num + "" ;
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -16,4 +16,19 @@ describe("fizzBuzz", () => {
   it("deberia mostrarme Buzz para el numero 5", () => {
     expect(fizzbuzz(5)).toEqual("Buzz");
   });
+  it("deberia mostrarme Fizz para multiplos de 3", () => {
+    expect(fizzbuzz(6)).toEqual("Fizz");
+  });
+  it("deberia mostrarme el numero 7", () => {
+    expect(fizzbuzz(7)).toEqual("7");
+  });
+  it("deberia mostrarme el numero 8", () => {
+    expect(fizzbuzz(8)).toEqual("8");
+  });
+  it("deberia mostrarme Fizz", () => {
+    expect(fizzbuzz(9)).toEqual("Fizz");
+  });
+  it("deberia mostrarme Buzz para el multiplo de 5", () => {
+    expect(fizzbuzz(10)).toEqual("Buzz");
+  });
 });
